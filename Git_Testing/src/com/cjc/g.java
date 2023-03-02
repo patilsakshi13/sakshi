d5636b24794837e1369de3b6a8726c8d81ab8868
HP <HP@192.168.100.10>
2023-03-02 13:53:12 +0530

    By smita

---
+++ Git_Testing/src/com/cjc/g.java
@@ -0,0 +1,25 @@
+package com.cjc;
+
+public class g 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		System.out.println("Hi");
+		System.out.println("Ho"); 
+		
+	}
+	public void m1()
+	{
+		System.out.println("vdfgdhgcg");
+		
+		
+	}
+	public void m2()
+	{
+		
+		System.out.println("vcsadhghg");
+	}
+
+
+}
